cc270700f737ab28c212feb166c7dd24d88a65cb
Szymon Karolczuk <szymonk1101@gmail.com>
2019-08-20 18:07:28 +0200

    index view

--- application/views/admin/partials/header.php
+++ application/views/admin/partials/header.php
@@ -811,14 +811,14 @@
                     <div class="app-sidebar__inner">
                         <ul class="vertical-nav-menu">
                             <li class="app-sidebar__heading">Menu</li>
-                            <li
-                                 class="mm-active"
-                                
-                                
-                                
-                                
-                                
-                            >
+
+                            <li>
+                                <a href="<?= base_url('admin'); ?>" class="mm-active">
+                                    <i class="metismenu-icon pe-7s-home"></i> Kokpit
+                                </a>
+                            </li>
+
+                            <li>
                                 <a href="#">
                                     <i class="metismenu-icon pe-7s-rocket"></i>
                                     Dashboards
