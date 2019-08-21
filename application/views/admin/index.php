f44a8fb37bcaadfa5f768b8cc536c8f186b8452d
Szymon Karolczuk <szymonk1101@gmail.com>
2019-08-21 21:46:14 +0200

    index calendar

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -235,66 +235,78 @@
 
                         <div class="row">
                             <div class="col-md-6">
-                                <div class="main-card card">
-                                    <div class="card-header">Aktualnie w pracy</div>
-                                    <div class="table-responsive">
-                                        <table class="align-middle mb-0 table table-borderless table-striped table-hover">
-                                            <thead>
-                                                <tr>
-                                                    <th>Name</th>
-                                                    <th class="text-center">Status</th>
-                                                    <th class="text-center">Sales</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>
-                                                        <div class="widget-content p-0">
-                                                            <div class="widget-content-wrapper">
-                                                                <div class="widget-content-left mr-3">
-                                                                    <div class="widget-content-left">
-                                                                        <img width="40" class="rounded-circle" src="assets/images/avatars/4.jpg" alt="">
+                                <div class="main-card mb-3 card">   
+                                    <div class="card-body">
+                                        <h5 class="card-title">Kalendarzyk</h5>
+                                        <div id="calendar-list"></div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="main-card mb-3 card">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Aktualnie w pracy</h5>
+                                        <div class="table-responsive">
+                                            <table class="align-middle mb-0 table table-borderless table-striped table-hover">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Name</th>
+                                                        <th class="text-center">Status</th>
+                                                        <th class="text-center">Sales</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>
+                                                            <div class="widget-content p-0">
+                                                                <div class="widget-content-wrapper">
+                                                                    <div class="widget-content-left mr-3">
+                                                                        <div class="widget-content-left">
+                                                                            <img width="40" class="rounded-circle" src="assets/images/avatars/4.jpg" alt="">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="widget-content-left flex2">
+                                                                        <div class="widget-heading">John Doe</div>
+                                                                        <div class="widget-subheading opacity-7">Web Developer</div>
                                                                     </div>
                                                                 </div>
-                                                                <div class="widget-content-left flex2">
-                                                                    <div class="widget-heading">John Doe</div>
-                                                                    <div class="widget-subheading opacity-7">Web Developer</div>
-                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </td>
-                                                    <td class="text-center">
-                                                        <div class="badge badge-warning">Zajęty</div>
-                                                    </td>
-                                                    <td class="text-center" style="width: 150px;">
-                                                        <div class="pie-sparkline">2,4,6,9,4</div>
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        <div class="widget-content p-0">
-                                                            <div class="widget-content-wrapper">
-                                                                <div class="widget-content-left mr-3">
-                                                                    <div class="widget-content-left">
-                                                                        <img width="40" class="rounded-circle" src="assets/images/avatars/3.jpg" alt="">
+                                                        </td>
+                                                        <td class="text-center">
+                                                            <div class="badge badge-warning">Zajęty</div>
+                                                        </td>
+                                                        <td class="text-center" style="width: 150px;">
+                                                            <div class="pie-sparkline">2,4,6,9,4</div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>
+                                                            <div class="widget-content p-0">
+                                                                <div class="widget-content-wrapper">
+                                                                    <div class="widget-content-left mr-3">
+                                                                        <div class="widget-content-left">
+                                                                            <img width="40" class="rounded-circle" src="assets/images/avatars/3.jpg" alt="">
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="widget-content-left flex2">
+                                                                        <div class="widget-heading">Ruben Tillman</div>
+                                                                        <div class="widget-subheading opacity-7">Etiam sit amet orci eget</div>
                                                                     </div>
                                                                 </div>
-                                                                <div class="widget-content-left flex2">
-                                                                    <div class="widget-heading">Ruben Tillman</div>
-                                                                    <div class="widget-subheading opacity-7">Etiam sit amet orci eget</div>
-                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </td>
-                                                    <td class="text-center">
-                                                        <div class="badge badge-success">Wolny</div>
-                                                    </td>
-                                                    <td class="text-center" style="width: 150px;">
-                                                        <div id="sparkline-chart4"></div>
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
+                                                        </td>
+                                                        <td class="text-center">
+                                                            <div class="badge badge-success">Wolny</div>
+                                                        </td>
+                                                        <td class="text-center" style="width: 150px;">
+                                                            <div id="sparkline-chart4"></div>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
